9bb529a0491c05eac2a7f44e810acb18d4e090b1
Victor <87928341+VMSandoval25@users.noreply.github.com>
2023-01-22 12:49:03 -0800

    First commit

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id.startsWith("com.google.cloud.tools.appengine")) {
+                useModule("com.google.cloud.tools:appengine-gradle-plugin:${requested.version}")
+            }
+        }
+    }
+}
+
+rootProject.name = "PeopleInSpace"
+
+include(":app", ":common")
+include(":compose-web")
+include(":compose-ios")
+include(":web")
+include(":backend")
+include(":graphql-server")
